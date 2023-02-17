f243a99d50e3f384fe6c1b5f1fb24b47949be16e
ki0lp <914685907@qq.com>
2023-02-17 23:33:43 +0800

    Initial commit

---
+++ ki-apps/ki-crk/src/main/java/com/ki0/CrkApplication.java
@@ -0,0 +1,11 @@
+package com.ki0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrkApplication.class, args);
+    }
+}
